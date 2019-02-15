3a6fbe623b5a56a78f837a865011fc13192ea215
fengyang <751498647@qq.com>
2019-02-15 14:30:11 +0800

    生日报表模板上传

---
+++ function/FlexFormula_GET_DATA.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+__author__ = "fengyang"
+
+class DynamicGetData(BaseFormulaObject):
+    def do(self, day, data):
+        current_date = self.context.get('CURRENT_DATE')
+        year_month_list = current_date.split("-")
+        _day, last_day = calendar.monthrange(int(year_month_list[0]), int(year_month_list[1]))
+        if day > last_day:
+            return ""
+        else:
+            return data
---
+++ function/FlexFormula_JUDGE_DATE.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = "fengyang"
+
+class DynamicGetDate(BaseFormulaObject):
+    def do(self, day):
+        year_month_list = self.context.get('CURRENT_DATE')
+        year = year_month_list.split("-")[0]
+        month = year_month_list.split("-")[1]
+        w_day, last_day = calendar.monthrange(int(year), int(month))
+        if int(day) > last_day:
+            return ""
+        else:
+            return str(day) + "号"
